806c07cedf8df13c6dd72c7129332a960fdb23b8
Bohdan Hodovanets <godov.b@gmail.com>
2026-05-15 15:20:02 +0300

    Add barCornerRadius property to CandleChartDataSetProtocol
    Added a new property for bar corner radius in the CandleChartDataSetProtocol.

--- Source/Charts/Data/Interfaces/CandleChartDataSetProtocol.swift
+++ Source/Charts/Data/Interfaces/CandleChartDataSetProtocol.swift
@@ -22,6 +22,10 @@ public protocol CandleChartDataSetProtocol: LineScatterCandleRadarChartDataSetPr
     /// the space that is left out on the left and right side of each candle,
     /// **default**: 0.1 (10%), max 0.45, min 0.0
     var barSpace: CGFloat { get set }
+
+    /// the corner radius of the candle bar,
+    /// **default**: 0.0
+    var barCornerRadius: CGFloat { get set }
     
     /// should the candle bars show?
     /// when false, only "ticks" will show
